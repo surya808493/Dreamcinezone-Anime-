85ca937c4670aa8cffbec0f626b90096876431ff
DreamXBotz <148682315+DreamXBotz@users.noreply.github.com>
2025-07-14 23:15:50 +0530

    Season Pagination Fix ✅

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -582,9 +582,7 @@ async def seasons_cb_handler(client: Client, query: CallbackQuery):
 @Client.on_callback_query(filters.regex(r"^fs#"))
 async def filter_seasons_cb_handler(client: Client, query: CallbackQuery):
     _, seas, key = query.data.split("#")
-    
-    offset = 0 # Initial offset for a new season filter.
-
+    offset = 0 
     curr_time = datetime.now(pytz.timezone('Asia/Kolkata')).time()
     search = FRESH.get(key).replace("_", " ")
     original_search = search
@@ -699,6 +697,7 @@ async def filter_seasons_cb_handler(client: Client, query: CallbackQuery):
         cur_time = datetime.now(pytz.timezone('Asia/Kolkata')).time()
         time_difference = timedelta(hours=cur_time.hour, minutes=cur_time.minute, seconds=(cur_time.second+(cur_time.microsecond/1000000))) - timedelta(hours=curr_time.hour, minutes=curr_time.minute, seconds=(curr_time.second+(curr_time.microsecond/1000000)))
         remaining_seconds = "{:.2f}".format(time_difference.total_seconds())
+        total_results = len(files)
         cap = await get_cap(settings, remaining_seconds, files, query, total_results, search)
         try:
             await query.message.edit_text(text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
